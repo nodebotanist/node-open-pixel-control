e878e389357537a0c07e6db19709387f2dffe194
Kassandra Perch <kassandra@kassandraperch.com>
2014-04-22 22:57:36 -0500

    HELL YEAH. Got a basic ping working.

---
+++ index.js
@@ -0,0 +1,37 @@
+
+//lets just see if this works.
+
+//String.fromCharCode(r);
+
+
+var len_hi_byte = parseInt(((20 * 3)  / 256), 10);
+var len_lo_byte = (20 * 3) % 256
+var header = String.fromCharCode(0) + String.fromCharCode(0) + String.fromCharCode(len_hi_byte) + String.fromCharCode(len_lo_byte);
+var colors = '';
+var pixels = new Uint8ClampedArray(64);
+for(var i = 0; i < 60; i+=3){
+	pixels[i] = 0;
+	pixels[i+1] = 255;
+	pixels[i+2] = 0
+}
+console.log(pixels.buffer.toString())
+var message = new Buffer(header);
+message = Buffer.concat([message, new Buffer(pixels)])
+// console.log(message);
+
+
+var net = require('net');
+var client = net.connect({port: 7890},
+    function() { //'connect' listener
+  console.log('client connected');
+  client.write(message);
+  console.log(message);
+  client.end();
+});
+client.on('data', function(data) {
+  console.log(data.toString());
+  client.end();
+});
+client.on('end', function() {
+  console.log('client disconnected');
+});
