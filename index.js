5ce5df57a87dcea574526af083735e7bf77b7f5c
Kassandra Perch <kassandra@kassandraperch.com>
2014-04-23 08:11:10 -0500

    Tweaks

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var len_hi_byte = parseInt(((20 * 3)  / 256), 10);
 var len_lo_byte = (20 * 3) % 256
 var header = String.fromCharCode(0) + String.fromCharCode(0) + String.fromCharCode(len_hi_byte) + String.fromCharCode(len_lo_byte);
 var colors = '';
-var pixels = new Uint8ClampedArray(64);
-for(var i = 0; i < 60; i+=3){
+var pixels = new Uint8ClampedArray(128);
+for(var i = 0; i < 128; i+=3){
 	pixels[i] = 0;
-	pixels[i+1] = 255;
+	pixels[i+1] = 258;
 	pixels[i+2] = 0
 }
 console.log(pixels.buffer.toString())
